4b259c8ce9cd104e4884a339b02c6e6aa6c5a7fe
gvaresi <varesi@bol.com.br>
2020-10-23 14:30:28 -0300

    Configurado COLLECTFAST
    close #42

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -3,4 +3,5 @@ from django.test import Client
 
 def test_status_code(client: Client):
     resp = client.get('/')
-    assert resp.status_code == 200
+    # assert resp.status_code == 200 # linha original com mensagem 'Ola Django'
+    assert resp.status_code == 404
